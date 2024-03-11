f7ca1058bf7d35819ce01b5dea356153f237701a
Dorina24 <euseidm@gmail.com>
2024-03-11 17:23:02 +0200

    Prima parte de exercitii

---
+++ script.js
@@ -0,0 +1,50 @@
+console.log(`Hello, ladies!`)   // test consola
+
+let varsta = 34;
+console.log(`Varsta mea este ${varsta} de ani.`);
+
+//suma a doua numere
+let numarA = 12;
+let numarB = 7;
+let suma = numarA + numarB;
+console.log(`Suma dintre ${numarA} si ${numarB} este ${suma}.`);
+
+//temperatura
+let temperaturaCelsius = 15;
+let temperaturaFahrenheit = temperaturaCelsius * 9/5 + 32;
+console.log(`Temperatura de ${temperaturaCelsius} grade Celsius este echivalenta cu temperatura de ${temperaturaFahrenheit}.`);
+
+// Hello + nume
+const numeleMeu = 'Andra';
+console.log(`Hello, ${numeleMeu}!`);
+
+// verificare numar par sau impar 
+let numarVerificat = 13;
+let rest = numarVerificat %2;
+let estePar = rest === 0;
+console.log(`Numarul ${numarVerificat} este par: ${estePar}.`);
+
+//variabila booleana
+let suntStudenta = false;
+console.log(`Sunt studenta = ${suntStudenta}`);
+
+//concatenare 2 siruri de caractere
+let primulSir = 'Cluj';
+let alDoileaSir = "Napoca";
+let liniuta = '-';
+console.log(`Exemplu concatenare siruri de caractere: ${sum=primulSir+liniuta+alDoileaSir}`);
+
+// afisare restul impartirii
+let restImpartire = numarA % numarB;
+console.log(`Restul impartirii lui ${numarA} la ${numarB} este ${restImpartire}.`);
+
+//declararea unei variabile undefined
+let neInitializat;
+console.log(`neInitializat = ${neInitializat}`);
+
+//aria unui dreptunghi
+let latime = 3;
+let lungime = 5;
+let aria = latime * lungime;
+console.log(`Aria dreptunghiului cu latimea de 3 si lungimea de 5 este ${aria}.`);
+
