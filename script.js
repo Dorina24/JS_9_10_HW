df6b8e158b27473cf651e0d26a55803a14bec276
Dorina24 <euseidm@gmail.com>
2024-03-12 15:02:02 +0200

    Tema modul ( + modul 10

--- script.js
+++ script.js
@@ -98,5 +98,80 @@ console.log(`Distanta de ${numarKm} km convertita in mile este: ${numarKm*0.6213
 let numarNul = null;
 console.log(typeof numarNul); 
 
+//declarare si comparatie 2 numere
+let num1 = 5;
+let num2 = 7;
+let comparatieNumere = num1 == num2;
+console.log(`Numerele ${num1} si ${num2} sunt egale => ${comparatieNumere}`);
+
+// discount
+let pretInitial = 35;
+let discount = 0.2;   //reprezinta 10%
+let pretRedus = pretInitial - (pretInitial*discount)
+console.log(`Pretul produsului dupa aplicarea reducerii este de ${pretRedus} de lei.`);
+
+
+//////////   JS 10  ///////////
+
+//Creează două array-uri: fructe = ['măr', 'portocală'] și 
+//legume = ['morcov', 'broccoli']. Concatenează-le într-un singur array numit alimente;
+let fructe = ['măr', 'portocală'];
+let legume = ['morcov', 'broccoli']; 
+let alimente = fructe+legume;
+alimente =fructe.concat(legume);
+console.log(alimente);
+
+// Creează un array de cuvinte: fructe = ['măr', 'portocală', 'banana'] 
+//verifica dacă ‘struguri’ se regaseste in array (indexOf) ;
+fructe = ['măr', 'portocală', 'banana'];
+let index = fructe.indexOf('struguri');
+console.log(index);
+
+// Creează un obiect Date și afișează anul curent în consolă;
+let currentDate = new Date();
+let year = currentDate.getFullYear();
+console.log(year);
+
+// Creează un obiect Date și adaugă 5 zile la data curentă. Afișează noua dată în consolă;
+let dataCurenta = new Date();
+let data =currentDate.getDate();
+let viitor = data + 5;
+console.log(`Peste 5 zile va fi data de ${viitor}`);
+
+//Creează două obiecte Date reprezentând două momente în timp (timestamp).
+//Calculează diferența dintre ele în milisecunde și afișează rezultatul în consolă
+let timestamp1 = new Date('2023-08-12').getTime();
+let timestamp2 = new Date('2024-08-12').getTime();
+let diferentaInMilisecunde = timestamp2 - timestamp1;
+console.log(diferentaInMilisecunde);
+
+
+//Creează un obiect cu informații despre tine: 
+//persoana = { nume: 'NumeleTau', varsta: 25, oras: 'OrasulTau' }. 
+//Afiseaza-l in consola;
+let andra = {
+    numeleMeu: "Dorina",
+    varstaMea: 34,
+    oras: "Cluj-Napoca",
+    }
+console.log(andra);
+
+//Modifică vârsta persoanei din obiectul creat anterior. 
+//Afișează obiectul modificat în consolă.
+andra.varstaMea = 18;
+console.log(andra.varstaMea);
+
+//Creează un array de obiecte, fiecare reprezentând o carte: 
+//carti = [{ titlu: 'Carte1', autor: 'Autor1', anPublicare: 2000 }, { titlu: 'Carte2', autor: 'Autor2', anPublicare: 1995 }]. 
+//Folosește o metoda a array-ului pentru a adauga încă un obiect reprezentand o carte.
+let carti = [{ titlu: 'Carte1', autor: 'Autor1', anPublicare: 2000 }, { titlu: 'Carte2', autor: 'Autor2', anPublicare: 1995 }];
+carti.push({titlu: 'Carte3', autor: 'Autor3', anPublicare: 2024 });
+console.log(carti);
+
+
+
+
+
+
 
 
